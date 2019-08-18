dfaba6321ebfdfb5dab72b4b2c97b302ec90dc5b
Basile <casou.acc@gmail.com>
2019-08-18 16:59:52 +0200

    Express server + css changes

---
+++ js/server/checkFiles.js
@@ -0,0 +1,49 @@
+const colors = require('./colors');
+const fs = require('fs');
+
+const PAD_LENGTH = 50;
+const STATUS = {
+  OK: `${colors.green}OK${colors.reset}`,
+  KO: `${colors.red}KO${colors.reset}`
+};
+
+
+const fileExist = (filePath) => {
+  return new Promise((resolve, reject) => {
+    fs.access(filePath, fs.F_OK, (err) => {
+      if (err) {
+        return reject({file: filePath, status: STATUS.KO, error: err});
+      }
+      resolve({filePath, status: STATUS.OK});
+    })
+  });
+};
+
+const pad = message => {
+  return message.padEnd(PAD_LENGTH, ' ').substr(0, PAD_LENGTH);
+};
+
+const _formatMessage = (message, status = STATUS.OK) => {
+  return `${pad(message)}\t${status}`;
+};
+
+function logResults(results) {
+  results.sort((a, b) => a.filePath.localeCompare(b.filePath))
+    .forEach(file => console.log(_formatMessage(`- ${file.filePath}`, file.status)));
+
+  if (results.filter(file => file.status !== STATUS.OK).length > 0) {
+    console.log(`${colors.yellow}WARNING : Some required files doesn't exists.${colors.reset}`)
+  }
+
+  return results;
+}
+
+const checkFiles = files => {
+  console.log(`${colors.bright}Check files${colors.reset}`);
+  return Promise.all(files.map(f => fileExist(f).catch(err => err)))
+    .then(logResults);
+};
+
+module.exports = {
+  checkFiles
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const colors = require('./js/server/colors');
+const { checkFiles } = require('./js/server/checkFiles');
+const PORT = 9999;
+const FILES_TO_CHECK = [
+  "resources/audio/audio-background.mp3",
+  "resources/audio/message.mp3",
+  "resources/audio/progress-bar-message.mp3",
+  "resources/videos/intro.mp4",
+  "resources/videos/final.mp4",
+  "resources/videos/never_gonna_give_you_up.mp4",
+  "resources/videos/nyan_cat.mp4",
+  "resources/videos/prairie.mp4",
+  "resources/videos/sax_guy.mp4"
+];
+
+console.log(`${colors.magenta}******* Starting server *******${colors.reset}`);
+
+const checkPromise = checkFiles(FILES_TO_CHECK);
+checkPromise.then((result) => {
+
+  const app = express();
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+  });
+
+  app.get('/regie', (req, res) => {
+    res.sendFile(path.join(__dirname + '/admin.html'));
+  });
+
+  app.use('/js', express.static(__dirname + '/js'));
+  app.use('/css', express.static(__dirname + '/css'));
+  app.use('/resources', express.static(__dirname + '/resources'));
+
+  app.listen(PORT);
+
+  console.log(`\n\n${colors.magenta}******* Started on port ${PORT} *******${colors.reset}\n`);
+  console.log(`http://localhost:${PORT} for the clock`);
+  console.log(`http://localhost:${PORT}/regie for the back office`);
+});
